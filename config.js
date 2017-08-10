5803116f592c41c61fbfc499c258c03ff51895d9
BakerStudio <billshark507@gmail.com>
2017-08-09 22:47:22 -0400

    add CI test and switch remote DB

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 exports.DATABASE_URL = process.env.DATABASE_URL ||
                        global.DATABASE_URL ||
-                      'mongodb://localhost:27017/interview';
-                      // 'mongodb://dbuser:dbpass@ds149437.mlab.com:49437/interview';
+                      // 'mongodb://localhost:27017/interview';
+                      'mongodb://dbuser:dbpass@ds149437.mlab.com:49437/interview';
 exports.PORT = process.env.PORT || 8080;
