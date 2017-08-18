a8d378b6f014894170c6d18980df29a22880389f
BakerStudio <billshark507@gmail.com>
2017-08-18 10:43:40 -0400

    display categories and counts

--- public/app.js
+++ public/app.js
@@ -23,12 +23,32 @@ function questionsList(data) {
   console.log("in questionsList function");
   for (var i = 0; i < data.length; i++) {
     console.log(data[i]);
-
   }
-
 }
 
+function listCategories(data) {
+  var text = '';
 
+  function categoryCount(catCountData) {
+    text = '<p><h2>Select a category:</h2></p>';
+    for (var i=0; i < data.length; i++) {
+      text = text + '<p><h3>' + data[i] + ' (' + catCountData[i];
+      if (catCountData[i] > 1) {
+        text = text + ' questions)</h3></p>';
+      } else {
+        text = text + ' question)</h3></p>';
+      }
+    }
+    $('.content').html(text);
+  }
+  $.getJSON(CATEGORY_COUNT_ENDPOINT, categoryCount);
+
+  // text = '<p><h2>Question categories - select one:</h2></p>';
+  // for (var i=0; i < data.length; i++) {
+  //   text = text + '<p>' + data[i] + '</p>';
+  // }
+  // $('.content').html(text);
+}
 
 
 $(function() {
@@ -36,8 +56,6 @@ $(function() {
   console.log("starting app...");
   switchEndpoints('remote');
 
-  // simple listing of all the questions, limited by limit set in server.js
-
-  $.getJSON(QUESTIONS_ENDPOINT,questionsList);
+  $.getJSON(CATEGORIES_ENDPOINT, listCategories);
 
 })
